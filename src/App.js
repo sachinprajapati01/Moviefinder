fcf0f867113d75f5b20001c10f759e97d7da27cd
Sachin <sachin@myself.com>
2022-02-02 22:39:41 +0530

    searched succesfully

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Movie from './component/Movie';
 import Navbar from './component/Navbar';
-const AP="http://www.omdbapi.com/?s=";
-const I="&apikey=61e74287";
+const API="http://www.omdbapi.com/?s=avenger&apikey=61e74287";
+
 function App() {
 
   const [movies,setMovies]=useState([]);
-  const[searchTerm,setSearchTerm]=useState(["avengers"]);
 
   useEffect(()=>{
-    fetch(AP+searchTerm+I)
+    fetch(API)
       .then((res)=>res.json())
         .then((data)=>{
+            console.log(data);
             setMovies(data.Search);
-            console.log({AP}+{searchTerm}+{I});
         })
   },[]);
 
   return (
     <div className="App">
-        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <Navbar movies={movies} setMovies={setMovies}/>
         {movies.length>0 && 
           movies.map((movie)=>
            <Movie key={movie.imdbID} {...movie}/>)
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
+const AP="http://www.omdbapi.com/?s="
+const I="&apikey=61e74287"
 
 export default function Navbar(props) {
 
+    const[searchTerm,setSearchTerm]=useState([]);
     const handleSubmit = (e) =>{
         e.preventDefault();
+        console.log("submit")
+        fetch(AP + searchTerm +I)
+        .then((res)=>res.json())
+        .then((data)=>{
+            props.setMovies(data.Search);
+        })
     }
     const handleChange =(e) =>{
-        props.setSearchTerm(e.target.value);
+        setSearchTerm(e.target.value);
+        console.log(searchTerm)
     }
   return <div style={{width:'99vw',background:'#aaafb1' , height:'60px',display:'flex',aligncontent:' center',justifyContent: 'space-between'}}>
         <p style={{paddingLeft:'33px',margin:'0px',postion:'absolute',color:'white' , fontFamily:'Cursive',fontWeight:'bold',fontSize:'40px'}}>MovieFinder</p>
@@ -18,7 +28,7 @@ export default function Navbar(props) {
         fontSize: '22px',
         textAlign: 'center',
     }} type='search' placeholder='Search...'
-        value={props.searchTerm}
+        value={searchTerm}
         onChange={handleChange}
     />
     </form>
